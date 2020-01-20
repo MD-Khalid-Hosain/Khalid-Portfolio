2f2e929ee45693d46899436b4d3f13530eb68ecc
MD-Khalid-Hosain <khalidkayes9@gmail.com>
2020-01-21 04:54:58 +0600

    upto team section

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*==============
+    Preloader jQuery
+==============*/
+$(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+
+/*==============
+    team owl-carousel jQuery
+==============*/
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 600,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false
+  });
+});
